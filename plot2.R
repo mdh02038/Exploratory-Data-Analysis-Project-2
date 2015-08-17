5fa10acc5c4a770c3a68214d3192a6b6abf51c67
Mark Hummel <mdh@raquette.com>
2015-08-16 21:24:19 -0400

    Plots 1 and 2 done

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 ## Exploratory Data Analysis
 ## Project 2
-## Plot1
+## Plot 2
 
 library(plyr)
 
@@ -9,12 +9,16 @@ path <- "exdata-data-NEI_data"
 NEI <- readRDS(paste(path,"summarySCC_PM25.rds",sep="/"))
 SCC <- readRDS(paste(path,"Source_Classification_Code.rds",sep="/"))
 
+## select rows for Baltimore City, Maryland (fips=="24510")
+select = NEI$fips=="24510"
+NEI_filtered <- NEI[select,]
+
 ## create data frame with summary of Emissions by year
-data <- ddply( NEI, .(year), numcolwise(sum) )
+data <- ddply( NEI_filtered, .(year), numcolwise(sum) )
 
 ## plot data
 plot( data$year, data$Emissions, type="l", ylab="Emissions", xlab="Year", main="Emissions per Year" )
 
 ## create png file
-dev.copy(png,"plot1.png")
+dev.copy(png,"plot2.png")
 dev.off()
